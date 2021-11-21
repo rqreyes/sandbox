a25f12fc8c33f3b0df4b5bcf06df980130523559
Randy Reyes <rqreyes@gmail.com>
2021-11-20 18:35:29 -0800

    fix: clean up configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { Typography } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
+import { Typography } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 
-import { Header } from './components/layout/Header';
-import { Welcome } from './pages/Welcome';
+import { Header } from "./components/layout/Header";
+import { Home } from "./pages/home";
+import { Welcome } from "./pages/Welcome";
 
 const App = () => (
   <>
     <Header />
     <Routes>
-      <Route path="/" element={<Welcome />}>
-        <Route
-          path="new-user"
-          element={<Typography variant="body1">herro!</Typography>}
-        />
+      <Route path="/" element={<Home />} />
+      <Route path="/welcome" element={<Welcome />}>
+        <Route path="new-user" element={<Typography variant="body1">herro!</Typography>} />
       </Route>
       <Route path="/create-book" element={<h1>create book</h1>} />
       <Route path="/update-book/:id" element={<h1>update book</h1>} />
