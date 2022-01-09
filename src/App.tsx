bffd783252fc971d09d2fb2cc2598a1bc026476a
Randy Reyes <rqreyes@gmail.com>
2022-01-08 18:29:34 -0800

    feat: add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Typography } from "@mui/material";
 import { Header } from "components/layout/Header";
+import { Login } from "components/page-login";
 import { Home } from "pages/Home";
-import { Welcome } from "pages/Welcome";
 import { Route, Routes } from "react-router-dom";
 
 const App = (): JSX.Element => (
@@ -9,15 +8,9 @@ const App = (): JSX.Element => (
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/welcome" element={<Welcome />}>
-        <Route
-          path="new-user"
-          element={<Typography variant="body1">herro!</Typography>}
-        />
-      </Route>
       <Route path="/create-book" element={<h1>create book</h1>} />
       <Route path="/update-book/:id" element={<h1>update book</h1>} />
-      <Route path="/login" element={<h1>login</h1>} />
+      <Route path="/login" element={<Login />} />
       <Route path="*" element={<h1>404 page</h1>} />
     </Routes>
   </>
---
+++ src/components/page-home/BookList.tsx
@@ -0,0 +1,38 @@
+import { Alert } from "@mui/material";
+import axios from "axios";
+import { Loading } from "components/generic/Loading";
+import React from "react";
+import { useQuery } from "react-query";
+
+interface BookItem {
+  author: string;
+  id: string;
+  title: string;
+}
+
+export const BookList = (): JSX.Element => {
+  const { data, error, isLoading } = useQuery(
+    "books",
+    async (): Promise<Array<BookItem>> => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_SERVER}/books`
+      );
+
+      return data;
+    }
+  );
+
+  if (isLoading) return <Loading />;
+  if (error instanceof Error)
+    return (
+      <Alert severity="error">{`An error has occurred: ${error.message}`}</Alert>
+    );
+
+  return (
+    <section>
+      {React.Children.toArray(
+        data?.map((bookItem: BookItem) => <p>{bookItem.author}</p>)
+      )}
+    </section>
+  );
+};
