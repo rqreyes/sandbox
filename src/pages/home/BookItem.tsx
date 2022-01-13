79d938c25a1ce1d105c9099a7b1ff798edca168d
Randy Reyes <rqreyes@gmail.com>
2022-01-13 01:58:02 -0800

    feat: add update book form

--- src/pages/home/BookItem.tsx
+++ src/pages/home/BookItem.tsx
@@ -8,33 +8,63 @@ import {
 } from "@mui/material";
 import axios, { AxiosResponse } from "axios";
 import { useSnackbar } from "notistack";
-import { useMutation, useQueryClient } from "react-query";
+import { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 import { BookItemData } from "./BookList";
+import { BookUpdateDialog } from "./BookUpdateDialog";
 
 export const BookItem: React.FC<BookItemData> = ({
   author,
   id,
   title,
 }): JSX.Element => {
+  const [isOpenUpdate, setIsOpenUpdate] = useState(false);
   const queryClient = useQueryClient();
   const { enqueueSnackbar } = useSnackbar();
-  const { error, isLoading, mutate } = useMutation<
+  // get book item for updating
+  const {
+    data: dataGet,
+    error: errorGet,
+    isFetching: isFetchingGet,
+    refetch: refetchGet,
+  } = useQuery<BookItemData, Error>(
+    "bookItem",
+    async (): Promise<BookItemData> => {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_SERVER}/books/${id}`
+      );
+
+      return data;
+    },
+    {
+      enabled: false,
+      refetchOnWindowFocus: false,
+    }
+  );
+  // delete book item
+  const { isLoading: isLoadingDelete, mutate: mutateDelete } = useMutation<
     AxiosResponse,
     Error,
     string
   >((id) => axios.delete(`${process.env.REACT_APP_API_SERVER}/books/${id}`), {
+    onError: (error) => {
+      enqueueSnackbar(`An error has occurred: ${error.message}`, {
+        variant: "error",
+      });
+    },
     onSuccess: () => {
       queryClient.invalidateQueries("bookList");
       enqueueSnackbar("Book deleted successfully", { variant: "success" });
     },
   });
-
-  if (error) {
-    enqueueSnackbar(`An error has occurred: ${error.message}`, {
-      variant: "error",
-    });
-  }
+  const handleOpenUpdate = () => {
+    refetchGet();
+    setIsOpenUpdate(true);
+  };
+  const handleCloseUpdate = () => {
+    setIsOpenUpdate(false);
+  };
 
   return (
     <>
@@ -44,14 +74,23 @@ export const BookItem: React.FC<BookItemData> = ({
           <br />
           {author}
         </ListItemText>
-        <Button>
+        <Button onClick={handleOpenUpdate}>
           <Edit />
         </Button>
-        <Button onClick={() => mutate(id)}>
-          {isLoading ? <CircularProgress size={20} /> : <Delete />}
+        <Button onClick={() => mutateDelete(id)}>
+          {isLoadingDelete ? <CircularProgress size={20} /> : <Delete />}
         </Button>
       </ListItem>
       <Divider />
+
+      <BookUpdateDialog
+        handleCloseUpdate={handleCloseUpdate}
+        dataGet={dataGet}
+        errorGet={errorGet}
+        id={id}
+        isFetchingGet={isFetchingGet}
+        isOpenUpdate={isOpenUpdate}
+      />
     </>
   );
 };
---
+++ src/pages/home/BookUpdateDialog.tsx
@@ -0,0 +1,124 @@
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  TextField,
+} from "@mui/material";
+import axios, { AxiosResponse } from "axios";
+import { Error } from "components/Error";
+import { useSnackbar } from "notistack";
+import { useEffect } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
+
+import { BookItemData, FormInput } from "./BookList";
+
+interface BookUpdateDialogProps {
+  dataGet: BookItemData | undefined;
+  errorGet: Error | null;
+  handleCloseUpdate: () => void;
+  id: string;
+  isFetchingGet: boolean;
+  isOpenUpdate: boolean;
+}
+
+export const BookUpdateDialog: React.FC<BookUpdateDialogProps> = ({
+  dataGet,
+  errorGet,
+  handleCloseUpdate,
+  id,
+  isFetchingGet,
+  isOpenUpdate,
+}) => {
+  const { enqueueSnackbar } = useSnackbar();
+  const queryClient = useQueryClient();
+  const defaultValues = {
+    author: "",
+    title: "",
+  };
+  const { control, handleSubmit, reset } = useForm({ defaultValues });
+  // update book item
+  const { isLoading: isLoadingUpdate, mutate: mutateUpdate } = useMutation<
+    AxiosResponse,
+    Error,
+    FormInput
+  >(
+    (data) =>
+      axios.put(`${process.env.REACT_APP_API_SERVER}/books/${id}`, data),
+    {
+      onError: (error) => {
+        enqueueSnackbar(`An error has occurred: ${error.message}`, {
+          variant: "error",
+        });
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries("bookList");
+        reset(defaultValues);
+        handleCloseUpdate();
+        enqueueSnackbar("Book updated successfully", { variant: "success" });
+      },
+    }
+  );
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    mutateUpdate(data);
+  };
+
+  useEffect(() => {
+    if (dataGet) reset({ author: dataGet.author, title: dataGet.title });
+  }, [dataGet, reset]);
+
+  return (
+    <Dialog onClose={handleCloseUpdate} open={isOpenUpdate}>
+      {/* handle error */}
+      {errorGet && <Error error={errorGet} />}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <DialogTitle>Update Book</DialogTitle>
+        {isFetchingGet ? (
+          <Grid alignItems="center" container justifyContent="center" pb={4}>
+            <CircularProgress sx={{ height: "100px" }} />
+          </Grid>
+        ) : (
+          <>
+            <DialogContent>
+              <Controller
+                control={control}
+                name="title"
+                render={({ field }) => (
+                  <TextField
+                    fullWidth
+                    label="Title"
+                    variant="standard"
+                    {...field}
+                  />
+                )}
+              />
+              <Controller
+                control={control}
+                name="author"
+                render={({ field }) => (
+                  <TextField
+                    fullWidth
+                    label="Author"
+                    variant="standard"
+                    {...field}
+                  />
+                )}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button type="submit">
+                {isLoadingUpdate ? <CircularProgress size={20} /> : "Update"}
+              </Button>
+              <Button onClick={handleCloseUpdate}>Cancel</Button>
+            </DialogActions>
+          </>
+        )}
+      </form>
+    </Dialog>
+  );
+};
