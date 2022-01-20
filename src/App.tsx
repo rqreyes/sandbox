2934240c0cc231138bde2d44dc7175f4440d154e
Randy Reyes <rqreyes@gmail.com>
2022-01-19 23:58:03 -0800

    feat: add book info page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Typography } from "@mui/material";
 import { Header } from "components/Header";
 import { Layout } from "components/Layout";
+import { BookInfo } from "pages/book-info/BookInfo";
 import { Home } from "pages/home";
 import { Login } from "pages/login";
 import { Route, Routes } from "react-router-dom";
@@ -11,7 +12,7 @@ const App = (): JSX.Element => (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/book-info/:id" element={<h1>Book Info</h1>} />
+        <Route path="/book-info/:id" element={<BookInfo />} />
         <Route
           path="/blank"
           element={<Typography variant="h1">Blank</Typography>}
