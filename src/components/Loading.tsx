2a68b2775637b16b5e8f87de72a98aa6ef5094d0
Randy Reyes <rqreyes@gmail.com>
2022-01-31 21:12:53 -0800

    feat: clean slate

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Backdrop, CircularProgress } from "@mui/material";
-
-export const Loading = (): JSX.Element => {
-  return (
-    <Backdrop open>
-      <CircularProgress color="inherit" />
-    </Backdrop>
-  );
-};
--- src/pages/login/LoginPage.tsx
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-
-export const Login = (): JSX.Element => (
-  <section>
-    <h1>Login</h1>
-    <Link to="/">Sign in</Link>
-  </section>
-);
