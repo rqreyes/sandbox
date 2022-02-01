3c0833591f9f5bbd0968fe2870e336fe027f8400
Randy Reyes <rqreyes@gmail.com>
2022-01-31 21:12:53 -0800

    feat: clean slate

--- src/components/Error.tsx
+++
@@ -1,9 +0,0 @@
-import { Alert } from "@mui/material";
-
-interface ErrorProps {
-  error: Error;
-}
-
-export const Error: React.FC<ErrorProps> = ({ error }): JSX.Element => (
-  <Alert severity="error">{`An error has occurred: ${error.message}`}</Alert>
-);
