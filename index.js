80be5a4a7b4f9ca37237af2768935a631b3069fc
Randy Reyes <rqreyes@gmail.com>
2022-02-10 01:19:38 -0800

    feat: add openapi specification

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ import cors from "cors";
 import morgan from "morgan";
 import { Low, JSONFile } from "lowdb";
 import { booksRouter } from "./routes/books.js";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+// initialize database
 const adapter = new JSONFile("db.json");
 const db = new Low(adapter);
 
+// add default data
 await db.read();
 db.data ||= { books: [] };
 await db.write();
-
 app.db = db;
+
+// set up API documentation
+const swaggerDocument = YAML.load("./spec/openapi.yaml");
+
+// set up middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use("/books", booksRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
